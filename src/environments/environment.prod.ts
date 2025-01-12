bbc3b17196d3c78ee26058226f40b9142457278e
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-01-12 08:36:25 -0500

    DevOps add deploy isolated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
     domain,
     clientId,
     ...(audience && audience !== "YOUR_API_IDENTIFIER" ? { audience } : null),
-    redirectUri: window.location.origin,
+    redirectUri: window.location.href,
     errorPath,
   },
   httpInterceptor: {
